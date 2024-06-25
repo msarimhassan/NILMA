1cca7619adb417a634b7fbe2bf96860704471790
Sarim Hassan <sarimhassan5@gmail.com>
2024-06-25 18:16:01 +0500

    updated the component index file

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { default as Button } from './button';
+export { default as Button } from './Button';
 export { default as Input } from './Input';
 export * from './text';
 export * from './spinners';
